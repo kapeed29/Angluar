ec4686341a3f86d70ef286f3d7c5ed4daea5f6d7
kapeed29 <deepakmale0002@gmail.com>
2021-06-12 16:17:58 +0530

    converted ts to js

---
+++ employee-test.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var employeeTest = /** @class */ (function () {
+  function employeeTest() {
+    id: 1;
+    name: "Deepak";
+    salary: 50000;
+    permanent: true;
+  }
+  return employeeTest;
+})();
+console.log(employeeTest);
