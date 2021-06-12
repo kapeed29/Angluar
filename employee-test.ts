bf20a9b74e5ecb6bef7d127c89a0f603d7ce0281
kapeed29 <deepakmale0002@gmail.com>
2021-06-12 16:06:58 +0530

    typescript

---
+++ employee-test.ts
@@ -0,0 +1,9 @@
+import { Employee } from "./employee";
+
+class employeeTest implements Employee {
+  id: 1;
+  name: "Deepak";
+  salary: 50000;
+  permanent: true;
+}
+console.log(employeeTest);
---
+++ employee.ts
@@ -0,0 +1,6 @@
+export interface Employee {
+  id: Number;
+  name: String;
+  salary: Number;
+  permanent: boolean;
+}
